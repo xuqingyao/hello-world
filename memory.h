a7d0fa9f961899776fa807d761e626aa5801ec9e
echo <33317148+xuqingyao@users.noreply.github.com>
2018-07-10 23:26:38 +0800

    Update memory.h

--- memory.h
+++ memory.h
@@ -37,55 +37,55 @@ public:
 		}
 		memory[pos++] = '\0';
 	}
-	void byte(int a) {
+	inline void byte(int a) {
 		memory[pos++] = a;
 	}
-	void half(short a) {
+	inline void half(short a) {
 		memory[pos++] = a >> 8;
 		memory[pos++] = a;
 	}
-	void word(int a) {
+	inline void word(int a) {
 		memory[pos++] = a >> 24;
 		memory[pos++] = a >> 16;
 		memory[pos++] = a >> 8;
 		memory[pos++] = a;
 	}
-	int space(int n) {
+	inline int space(int n) {
 		int tmp = pos;
 		pos += n;
 		return tmp;
 	}
-	int getPos() {
+	inline int getPos() {
 		return pos;
 	}
-	void save(int p, int len, int a) {
+	inline void save(int p, int len, int a) {
 		for (int i = 0; i < len; ++i)
 			memory[p + i] = a >> (8 * (len - i - 1));
 	}
-	void gotoPos(int p) {
+	inline void gotoPos(int p) {
 		pos = p;
 	}
-	int readWord(int p) {
+	inline int readWord(int p) {
 		int ans;
 		ans = memory[p + 3] + (memory[p + 2] << 8) + (memory[p + 1] << 16) + (memory[p] << 24);
 		return ans;
 	}
-	short readHalf(int p) {
+	inline short readHalf(int p) {
 		short ans;
 		ans = memory[p + 1] + (memory[p] << 8);
 		return ans;
 	}
-	int readByte(int p) {
+	inline int readByte(int p) {
 		return memory[p];
 	}
-	int getTop() {
+	inline int getTop() {
 		return pos - 1;
 	}
-	void print(int p) {
+	inline void print(int p) {
 		for (int i = p; memory[i] != '\0'; ++i)
 			std::cout << memory[i];
 	}
-	void saveString(std::string &str, int p, int len) {
+	inline void saveString(std::string &str, int p, int len) {
 		for (int i = 0; i < len; ++i)
 			memory[i + p] = str[i];
 	}
