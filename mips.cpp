ed3584d8c3a24265531b5563d1af1d247f925a01
echo <33317148+xuqingyao@users.noreply.github.com>
2018-07-12 10:37:05 +0800

    Update mips.cpp

--- mips.cpp
+++ mips.cpp
@@ -135,8 +135,8 @@ void processText();
 
 int main(int argc, char* argv[]) {
 	Register[29] = 4 * 1024 * 1024;
-	std::ifstream infile;
-	infile.open("2.s");
+	std::ifstream infile(argv[1]);
+	//infile.open("2.s");
 	std::string str, line, tmp;
 	std::string s[3];
 	initKey();
